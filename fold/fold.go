6ae7c9ba1273bb936e9a8be49710f91c940c80c2
Robert Weber <r.weber@f5.com>
2019-08-28 09:49:33 -0600

    Fix Licensing

--- fold/fold.go
+++ fold/fold.go
@@ -1,3 +1,7 @@
+// Copyright 2019 F5 Networks. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 // various functions that take sequences of things and combine them into one thing
 package fold
 
